a12dc0f3f73f80f829e9a46ac9b0d404c20f6aa7
YU LIN <118993869+linyu2703@users.noreply.github.com>
2025-12-02 22:03:39 -0500

    s

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // The 'base' property tells Vite to use this path as the root 
-  // for all assets (JS, CSS, etc.) when the project is built.
-  // This MUST match your GitHub repository name, enclosed in forward slashes.
-  base: '/portfolio-web/', 
-  
+  base: "/portfolio-web/",
   plugins: [react()],
-});
+})
